6f49c45295459562dc7d71a855a7b62d374f1191
Rene Saarsoo <nene@triin.net>
2010-02-15 19:22:42 +0200

    Added xmonad conf.

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,21 @@
+import XMonad
+import XMonad.Config.Kde
+import qualified XMonad.StackSet as W -- to shift and float windows
+ 
+main = xmonad $ kdeConfig
+ 
+ { modMask = mod4Mask -- use the Windows button as mod
+ , manageHook = manageHook kdeConfig <+> myManageHook
+ }
+ where
+   myManageHook = composeAll . concat $
+     [ [ className   =? c --> doFloat           | c <- myFloats]
+     , [ title       =? t --> doFloat           | t <- myOtherFloats]
+     , [ className   =? c --> doF (W.shift "3") | c <- webApps]
+     , [ className   =? c --> doF (W.shift "4") | c <- ircApps]
+     ]
+   myFloats      = ["MPlayer", "Gimp", "Skype", "Plasma-desktop", "VirtualBox"]
+   myOtherFloats = ["alsamixer"]
+   webApps       = ["Firefox-bin", "Opera"] -- open on desktop 3
+   ircApps       = ["Ksirc"]                -- open on desktop 4
+   
