1cf13fefe78b32e0627b7ac579cfa5385cab252e
Rene Saarsoo <nene@triin.net>
2010-06-14 14:57:13 +0300

    Removed webApps and ircApps from xmonad config.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -12,12 +12,8 @@ main = xmonad $ kdeConfig
    myManageHook = composeAll . concat $
      [ [ className   =? c --> doFloat           | c <- myFloats]
      , [ title       =? t --> doFloat           | t <- myOtherFloats]
-     , [ className   =? c --> doF (W.shift "3") | c <- webApps]
-     , [ className   =? c --> doF (W.shift "4") | c <- ircApps]
      , [ composeOne [ isFullscreen -?> doFullFloat ] ]
      ]
    myFloats      = ["MPlayer", "Gimp", "Skype", "Plasma-desktop", "VirtualBox"]
    myOtherFloats = ["alsamixer"]
-   webApps       = ["Firefox-bin", "Opera"] -- open on desktop 3
-   ircApps       = ["Ksirc"]                -- open on desktop 4
    
