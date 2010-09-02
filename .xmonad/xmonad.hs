d92abbbdd2d975ba9bb7658159cdf972e6bfec42
Rene Saarsoo <nene@triin.net>
2010-09-02 20:34:40 +0300

    Added fullscreen option to xmonad config.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,11 +2,14 @@ import XMonad
 import XMonad.Config.Kde
 import qualified XMonad.StackSet as W -- to shift and float windows
 import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders
+import XMonad.Config.Desktop (desktopLayoutModifiers)
 
 main = xmonad $ kdeConfig
  
  { modMask = mod4Mask -- use the Windows button as mod
  , manageHook = manageHook kdeConfig <+> myManageHook
+ , layoutHook = layoutHook kdeConfig ||| noBorders Full
  }
  where
    myManageHook = composeAll . concat $
