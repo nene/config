5d14913271337fe50257862803a00010d4bc4191
Rene Saarsoo <nene@triin.net>
2011-07-21 10:42:34 +0300

    Fix sdk-grep paths.

--- .emacs.d/js2mods.el
+++ .emacs.d/js2mods.el
@@ -193,4 +193,4 @@ when that line is empty, looks at the line before it etc."
 
 (defun sdk-grep (needle)
   (interactive "sFind SDK JS: ")
-  (grep-find (concat "find ~/work/SDK/extjs/src ~/work/SDK/platform/src ~/work/SDK/platform/src -iname '*.js' -print0 | xargs -0 -e grep -nH -e '" needle "'")))
+  (grep-find (concat "find ~/work/SDK/extjs/src ~/work/SDK/platform/src ~/work/SDK/platform/core/src -iname '*.js' -print0 | xargs -0 -e grep -nH -e '" needle "'")))
