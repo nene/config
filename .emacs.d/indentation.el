67511f57fe0f8cab232b40fdd53436223416bd00
Rene Saarsoo <nene@triin.net>
2014-02-17 09:49:06 +0200

    Enable JavaScript TABs indent only when in ~/work/sport/.

--- .emacs.d/indentation.el
+++ .emacs.d/indentation.el
@@ -23,14 +23,37 @@
     (indent-according-to-mode)))
 
 
+;; Helpers to work with strings
+
+(defun string/starts-with (s begins)
+  "returns non-nil if string S starts with BEGINS.  Else nil."
+  (cond ((>= (length s) (length begins))
+         (string-equal (substring s 0 (length begins)) begins))
+        (t nil)))
+
+(defun string/ends-with (s ending)
+  "return non-nil if string S ends with ENDING."
+  (cond ((>= (length s) (length ending))
+         (let ((elength (length ending)))
+           (string= (substring s (- 0 elength)) ending)))
+        (t nil)))
+
+
+(defun file-in-directory (path)
+  "True when current file is inside the specified directory."
+  (string/starts-with (or (buffer-file-name) default-directory) (file-truename path)))
+
+
 (add-hook 'ruby-mode-hook
   (lambda ()
 	(define-key ruby-mode-map (kbd "RET") 'newline-and-indent)
 	(define-key ruby-mode-map (kbd "TAB") 'indent-line-or-region)))
 
+;; Enable TABs only in Sportlyzer source
 (add-hook 'js2-mode-hook
   '(lambda ()
-    (set-variable 'indent-tabs-mode t)))
+     (if (file-in-directory "~/work/sport")
+         (set-variable 'indent-tabs-mode t))))
 
 (add-hook 'php-mode-hook
   (lambda ()
